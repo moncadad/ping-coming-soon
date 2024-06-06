20b4670c3a4790fbb0e1db80ae74a5caad093c4c
Denise <88467086+moncadad@users.noreply.github.com>
2024-06-06 18:18:03 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,10 @@
+/*
+
+The Challenge
+==================================================================================
+- Submit their email address using an `input` field
+- Receive an error message when the `form` is submitted if:
+	- The `input` field is empty. The message for this error should say *"Whoops! It looks like you forgot to add your email"*
+- The email address is not formatted correctly (i.e. a correct email address should have this structure: `name@host.tld`). The message for this error should say *"Please provide a valid email address"
+
+*/
